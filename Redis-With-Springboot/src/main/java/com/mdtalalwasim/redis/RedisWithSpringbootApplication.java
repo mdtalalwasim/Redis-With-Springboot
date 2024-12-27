3eded6b4292812f294daa11e2038a5887c582d36
Md. Talal Wasim <md.talalwasim@gmail.com>
2024-12-27 20:13:26 +0600

    very first commit for initial project setup.

---
+++ Redis-With-Springboot/src/main/java/com/mdtalalwasim/redis/RedisWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.mdtalalwasim.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisWithSpringbootApplication.class, args);
+	}
+
+}
